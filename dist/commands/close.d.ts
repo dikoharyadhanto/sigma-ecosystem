de9e0dd9c891c47034843664d65839eaea69fe1f
dikoharyadhanto <dikoharyadhanto74@gmail.com>
2026-05-17 18:52:04 +0700

    fix: robust build-on-install by moving ts to dependencies and using prepare script

--- dist/commands/close.d.ts
+++
@@ -1,3 +0,0 @@
-import { Command } from 'commander';
-export declare function closeCommand(): Command;
-//# sourceMappingURL=close.d.ts.map
--- dist/commands/roadmap.d.ts
+++
@@ -1,3 +0,0 @@
-import { Command } from 'commander';
-export declare function roadmapCommand(): Command;
-//# sourceMappingURL=roadmap.d.ts.map
